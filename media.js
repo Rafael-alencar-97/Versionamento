70b0070fba84a6a39ada050793161caaeae2ce86
Rafael-alencar-97 <alencarrafael1109@gmail.com>
2024-10-02 21:23:43 -0300

    codigo de media

---
+++ media.js
@@ -0,0 +1,14 @@
+let nota1 = 4
+let nota2 = 9
+let nota3 = 5
+
+let media = ((nota1 + nota2 + nota3) / 3)
+
+if (media >=6) {
+console.log ('aprovado') }
+
+    else { (media <=5)
+    console.log ('reprovado')
+    
+}
+ console.log (media);
